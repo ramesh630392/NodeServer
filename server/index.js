536ef89beaf79e0fdfe78e8e12163d7e4d0c1ae0
ramesh630392 <ramesh9912599@gmail.com>
2025-01-09 16:40:05 +0530

    added basic vercel

--- server/index.js
+++ server/index.js
@@ -1,9 +1,20 @@
 const express = require('express');
+const serverless = require('serverless-http'); // Converts Express to a serverless function
 
 const app = express();
 
-app.get('/test', (req, res)=>{
-    res.send("hii")
-})
+// Middleware to parse JSON
+app.use(express.json());
 
-app.listen(3001, ()=>{console.log("CN")})
+// Basic Routes
+app.get('/', (req, res) => {
+  res.status(200).send('Welcome to the Express server running on Vercel!');
+});
+
+app.get('/api/hello', (req, res) => {
+  res.status(200).json({ message: 'Hello from Express on Vercel!' });
+});
+
+// Export the server as a serverless function
+module.exports = app;
+module.exports.handler = serverless(app);
